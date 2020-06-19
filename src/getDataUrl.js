baaa8bbf432607278ff4176ce00acf1bab8e4398
Anke18 <uczlaw3@ucl.ac.uk>
2020-06-19 10:16:01 +0800

    fix bug of url

--- src/getDataUrl.js
+++ src/getDataUrl.js
@@ -4,14 +4,14 @@
  * @Author: Anke Wang
  * @Date: 2020-04-28 19:15:53
  * @LastEditors: Anke Wang
- * @LastEditTime: 2020-06-18 14:15:51
+ * @LastEditTime: 2020-06-19 10:08:12
  */
 //https://bigd.big.ac.cn/ncov/rest/variation/haplotype/json?date=UTR-29753&area=Beijing&frequency=0
 //https://bigd.big.ac.cn/ncov/rest/variation/haplotype/json?date=freq_0&area=world
 export const getFreUrl = () => {
 
     let freid = $('#fre').val();
-    let frestr = ["UTR-29753","UTR-29753","UTR-non"]
+    let frestr = ["UTR-29753","UTR-29870","UTR-non"]
     let furl = "https://bigd.big.ac.cn/ncov/rest/variation/haplotype/json?date="+frestr[freid]+"&area=Beijing&frequency=0";
     //console.log(furl);
     return furl;
