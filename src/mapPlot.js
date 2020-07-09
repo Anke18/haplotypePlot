af5432110629feb268619ad8fd1eeb3d42cb3691
Anke18 <uczlaw3@ucl.ac.uk>
2020-07-09 11:22:54 +0800

    modify latlng

--- src/mapPlot.js
+++ src/mapPlot.js
@@ -4,7 +4,7 @@
  * @Author: Anke Wang
  * @Date: 2020-04-04 15:31:42
  * @LastEditors: Anke Wang
- * @LastEditTime: 2020-07-06 15:18:52
+ * @LastEditTime: 2020-07-09 11:15:17
  * 
  * Code reference:
  * Leaflet Map: https://leafletjs.com/
@@ -130,7 +130,7 @@ export const setCountryCoord = () => {
         { "name": "Benin", "lat": 6.379448 , "lng": 2.451324 },
         { "name": "Venezuela", "lat": 10.500000 , "lng": 293.194444 },
         { "name": "Bulgaria", "lat": 42.698334 , "lng": 23.319941 },
-        { "name": "Peru", "lat": -10.01465 , "lng": -71.27194 },
+        { "name": "Peru", "lat": -10.01465 , "lng": 288.72806 },
     ];
 
     let getLatlng = {}
